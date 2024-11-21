986e0c76d8f47c18b9700760f2d3d2871f36c1f6
choihabin <chabin7181@gmail.com>
2024-11-22 00:56:41 +0900

    Feat : ItemShop 기능 추가
    - OnTriggerEnter로 플레이어를 인식하고, 마우스 우클릭 시 shopPanel 활성화 / 벗어날 시 비활성화
    - riceSeed/TomatoSeed 구매 버튼 클릭 시 Toolbar로 해당 아이템 추가 / 돈 차감

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using JetBrains.Annotations;
+
+public class Shop : MonoBehaviour
+{
+    public GameObject shopPanel;
+    public Button riceBuyBtn;
+    public Button tomatoBuyBtn;
+    public List<Item> sellItems;
+
+    private bool isPlayerInRange = false;
+    private Player player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<Player>();
+
+        riceBuyBtn.onClick.AddListener(() => BuyItem(sellItems[0]));
+        tomatoBuyBtn.onClick.AddListener(() => BuyItem(sellItems[1]));
+    }
+
+    private void Update()
+    {
+        if (isPlayerInRange && Input.GetMouseButtonDown(1))
+        {
+            shopPanel.SetActive(true);
+        }
+    }
+
+    public void BuyItem(Item item)
+    {
+        if (sellItems.Contains(item))
+        {
+            if (player.money >= item.itemData.price)
+            {
+                player.inventoryManager.Add("Toolbar", item);
+                StartCoroutine(InGameUI.instance.UpdateMoneyEffect(player.money, player.money - item.itemData.price));
+                Debug.Log(item.plantData.plantName);
+            }
+            else
+            {
+                InGameUI.instance.ShakingText();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            isPlayerInRange = false;
+            shopPanel.SetActive(false);
+        }
+    }
+
+}
