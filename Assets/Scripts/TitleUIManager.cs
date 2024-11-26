302eb6bda896fff4ccfb828c07eb345fe15b41c6
choihabin <chabin7181@gmail.com>
2024-11-27 02:26:41 +0900

    Revert "Chore : TitleUIManager -> OutGameUI rename"
    This reverts commit 145aecb7aec4c18a11a7ce69ba165c9b29b890ad.

---
+++ Assets/Scripts/TitleUIManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUIManager : MonoBehaviour
+{
+    public static TitleUIManager instance;
+
+    public GameObject panel;
+    public Button newGameBtn;
+    public Button continueGameBtn;
+    public Button exitGameBtn;
+    public Button yesBtn;
+    public Button noBtn;
+    public bool isNewGame;
+
+    void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        GameObject inventoryObj = new GameObject("InventorySave");
+        SaveData inventorySave = inventoryObj.AddComponent<SaveData>();
+
+        if (!inventorySave.HasSavedInventory())
+        {
+            isNewGame = true;
+            continueGameBtn.gameObject.SetActive(false);
+            newGameBtn.onClick.AddListener(() =>
+            {
+                FadeEffect.instance.FadeAndLoadScene("InGameScene");
+            });
+        }
+        else
+        {
+            isNewGame = false;
+            continueGameBtn.gameObject.SetActive(true);
+            continueGameBtn.onClick.AddListener(() =>
+            {
+                FadeEffect.instance.FadeAndLoadScene("InGameScene");
+            });
+
+            newGameBtn.onClick.AddListener(() =>
+            {
+                // 기존 리스너 제거 (중복 등록 방지)
+                yesBtn.onClick.RemoveAllListeners();
+                noBtn.onClick.RemoveAllListeners();
+
+                panel.SetActive(true);
+
+                yesBtn.onClick.AddListener(() => { isNewGame = true; FadeEffect.instance.FadeAndLoadScene("InGameScene"); });
+                noBtn.onClick.AddListener(() => { panel.SetActive(false); });
+            });
+        }
+
+        exitGameBtn.onClick.AddListener(() =>
+        {
+            Application.Quit();
+        });
+    }
+}
